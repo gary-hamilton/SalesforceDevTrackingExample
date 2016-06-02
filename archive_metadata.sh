b9c9472d8feac5c0bb1563fa082a3ccb1c641d73
Gary Hamilton <gary_hamilton@yahoo.com>
2016-06-02 16:53:40 -0400

    Add files via upload

---
+++ archive_metadata.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# check if git is initialized
+ls .git > /dev/null
+if [ $? != 0 ] ; then
+	git init
+fi
+BUILD_ID="$1" 
+echo "Build: $BUILD_ID"
+echo 	"git add *"
+git add * 
+echo 	"git commit -a -m \"$BUILD_ID\""
+git commit -a -m "$BUILD_ID"
+git tag $BUILD_ID
+git push archive
